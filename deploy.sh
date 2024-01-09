82974f3d661f85cb322524abc2242edbfdc11854
Arthur Guiot <arguiot@gmail.com>
2024-01-09 08:52:22 -0800

    Deployed on optimism

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 
 CONTRACT_PATH=src/MUWPTransfer.sol
 PRIVATE_KEY="f8487218c07526de64adff2a382d1bc9320738b8912187b5b27267b69761b3e8"
-HTTPS_ENDPOINT="https://arbitrum.llamarpc.com"
+HTTPS_ENDPOINT="https://optimism.llamarpc.com"
 
 
 forge create MUWPTransfer --contracts $CONTRACT_PATH --private-key $PRIVATE_KEY --rpc-url $HTTPS_ENDPOINT
