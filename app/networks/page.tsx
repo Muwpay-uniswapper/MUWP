b65ed81debfbee478cb07b73de580d3ecadbb8ef
Arthur Guiot <arguiot@gmail.com>
2024-01-13 12:32:42 -0800

    Fixed spelling

--- app/networks/page.tsx
+++ app/networks/page.tsx
@@ -2,8 +2,8 @@ import { Networks as NetworkClient } from "@/components/networks/networks";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: 'MUWP Pay - Networks',
-	description: 'Select the bridges and DEXs that you want to use with MUWP Pay.'
+	title: 'MUWPay - Networks',
+	description: 'Select the bridges and DEXs that you want to use with MUWPay.'
 }
 
 export default function Networks() {
