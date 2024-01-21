fec7622dee3c5e44d87c63b115fcb6f72cf1031d
Arthur Guiot <arguiot@gmail.com>
2024-01-20 17:07:46 -0800

    Deployed on ethereum

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 
 CONTRACT_PATH=./src/MUWPTransfer.sol
 PRIVATE_KEY="f8487218c07526de64adff2a382d1bc9320738b8912187b5b27267b69761b3e8"
-HTTPS_ENDPOINT="https://1rpc.io/zksync2-era"
-RUST_BACKTRACE=1
+HTTPS_ENDPOINT="https://eth.llamarpc.com"
 
-zkforge zk-create ./src/MUWPTransfer.sol:MUWPTransfer --chain 270 --private-key $PRIVATE_KEY --rpc-url $HTTPS_ENDPOINT
+
+forge create MUWPTransfer --use "0.8.22" --optimize --contracts $CONTRACT_PATH --private-key $PRIVATE_KEY --rpc-url $HTTPS_ENDPOINT
