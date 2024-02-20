dbe29a8348b4044b83b05e48a6f4b58201a4f479
Arthur Guiot <arguiot@gmail.com>
2024-02-19 17:11:44 -0800

    Fixed animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -93,12 +93,18 @@ module.exports = {
 				pulse: {
 					'0%': { transform: 'scale(1)', opacity: '1' },
 					'100%': { transform: 'scale(1.5)', opacity: '0.2' },
-				}
+				},
+				pulse2: {
+					'0%': { opacity: '1' },
+					'50%': { opacity: '0.2' },
+					'100%': { opacity: '1' },
+				},
 			},
 			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
 				pulse: 'pulse 500ms ease-out forwards',
+				pulse2: 'pulse2 1s ease-in-out infinite',
 			},
 		},
 	},
