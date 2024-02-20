d37199326ccd0103dafbcb0175b9ccb0db6bfcf3
Arthur Guiot <arguiot@gmail.com>
2024-02-19 17:04:26 -0800

    Added aptos redirect

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 const million = require('million/compiler');
+
 /** @type {import('next').NextConfig} */
-module.exports = million.next({
+const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
-}, {
+  redirects: async () => {
+    return [
+      {
+        source: '/aptos',
+        destination: '/?toChain=12360001',
+        permanent: true,
+      },
+    ];
+  }
+}
+module.exports = million.next(nextConfig, {
   auto: { rsc: true },
 });
