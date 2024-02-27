6207b3ee123bd8f6392002cee445fdd957006563
Arthur Guiot <arguiot@gmail.com>
2024-02-27 09:25:08 -0800

    Added tracking

--- app/api/receive-funds/route.ts
+++ app/api/receive-funds/route.ts
@@ -1,6 +1,7 @@
 import { EthereumAddress } from "@/lib/core/model/Address";
 import { inngest } from "@/lib/inngest/client";
 import { z } from "zod";
+import { track } from '@vercel/analytics/server';
 
 BigInt.prototype.toJSON = function () {
     return this.toString();
@@ -19,14 +20,19 @@ export async function POST(request: Request) {
             accountAddress: EthereumAddress,
         }).parse(body);
 
-        await inngest.send({
+        const inngestID = await inngest.send({
             name: "app/funds.transferred",
             data: {
                 transactionHash: input.transactionHash,
                 chainId: input.chainId,
                 address: input.accountAddress,
             },
-        })
+        });
+
+        await track('Swap started', {
+            ...input,
+            inngestID: inngestID.ids[0],
+        });
 
         return new Response(JSON.stringify({
             status: "success",
