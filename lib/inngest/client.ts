18aa78694d4b3776301201470a588dfd61b5136f
Arthur Guiot <arguiot@gmail.com>
2024-03-04 12:51:27 -0800

    Preparing for launch

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
 // Create a client to send and receive events
 export const inngest = new Inngest({
-    id: `muwp-${process.env.NODE_ENV}`,
+    id: `muwp`,
 });
