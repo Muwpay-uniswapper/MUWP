301eb63f2961870b8667b4483206db5b5831a8b8
Arthur Guiot <arguiot@gmail.com>
2024-10-04 12:06:00 -0700

    [wip] stellar

--- lib/layerzero/aptos/omnichains.ts
+++ lib/layerzero/aptos/omnichains.ts
@@ -32,6 +32,7 @@ export const OmnichainAptosBridge = {
 }
 
 export const AptosChainId = 12360001;
+export const StellarChainId = 1337;
 
 export const AptosTokensAddress = {
     usdc: "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC",
