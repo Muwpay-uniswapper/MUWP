5615324b16e5c43301045e7205e0837acf0f3c8d
Arthur Guiot <arguiot@gmail.com>
2024-10-31 10:22:37 -0700

    Improved AllBridge integration with Stellar

--- components/layout/FindRoutesButton.tsx
+++ components/layout/FindRoutesButton.tsx
@@ -9,115 +9,164 @@ import { toast } from "sonner";
 import React, { useState } from "react";
 import { zeroAddress } from "viem";
 
-
-
 export function FindRoutesButton() {
-    const { inputTokens, outputTokens, inputAmount, outputChain, allowDenyBridges, allowDenyExchanges, outputDistribution, targetAddress } = useSwapStore();
-    const { fetchRoutes, isFetching, tempAccount, validUntil, multiWalletDistribution, multiWallets, getRoutes, gasPayer } = useRouteStore();
-    const { address, chain } = useAccount();
-    const [trials, setTrials] = useState(0);
-    React.useEffect(() => {
-        const interval = setInterval(async () => {
-            if (process.env.NODE_ENV !== "production") return;
-            if (validUntil && validUntil < new Date() && !isFetching && trials <= 3) {
-                await onClick();
-            }
-        }, 1000);
+  const {
+    inputTokens,
+    outputTokens,
+    inputAmount,
+    outputChain,
+    allowDenyBridges,
+    allowDenyExchanges,
+    outputDistribution,
+    targetAddress,
+  } = useSwapStore();
+  const {
+    fetchRoutes,
+    isFetching,
+    tempAccount,
+    validUntil,
+    multiWalletDistribution,
+    multiWallets,
+    getRoutes,
+    gasPayer,
+  } = useRouteStore();
+  const { address, chain } = useAccount();
+  const [trials, setTrials] = useState(0);
+  React.useEffect(() => {
+    const interval = setInterval(async () => {
+      if (process.env.NODE_ENV !== "production") return;
+      if (validUntil && validUntil < new Date() && !isFetching && trials <= 3) {
+        await onClick();
+      }
+    }, 1000);
 
-        return () => clearInterval(interval);
-    }, [validUntil, isFetching]);
+    return () => clearInterval(interval);
+  }, [validUntil, isFetching]);
 
-    React.useEffect(() => {
-        if (address) {
-            useRouteStore.setState({ multiWallets: [address], gasPayer: address });
-        }
-    }, [address, typeof multiWallets == "undefined"]);
-
-    const _distribution = { ...multiWalletDistribution };
-
-    const fixDistribution = () => {
-        const routes = getRoutes();
-        const inputTokens = routes.map(route => ({ token: route.fromToken, fromAmount: route.fromAmount }));
-        const total = inputTokens.reduce((acc, { fromAmount }) => acc + BigInt(fromAmount), 0n);
-
-        for (const { token, fromAmount } of inputTokens) {
-            // Distribute the tokens evenly. The bigint represents the token amount, not the percentage
-            const amount = BigInt(fromAmount);
-            const wallets = multiWallets ?? [address!];
-            if (!_distribution[token.address]
-                || Object.keys(_distribution[token.address]).length !== wallets.length
-                || !wallets.every(wallet => Object.keys(_distribution[token.address]).includes(wallet))
-                || total !== Object.values(_distribution[token.address]).reduce((acc, val) => acc + val, 0n)
-            ) {
-                _distribution[token.address] = {};
-                if (token.address === zeroAddress) {
-                    _distribution[token.address][gasPayer ?? address!] = amount;
-                } else {
-                    let leftToSpend = amount;
-                    wallets.forEach((wallet, index) => {
-                        if (index === wallets.length - 1) {
-                            _distribution[token.address][wallet] = leftToSpend;
-                        } else {
-                            const share = amount / BigInt(wallets.length);
-                            _distribution[token.address][wallet] = share;
-                            leftToSpend -= share;
-                        }
-                    });
-                }
-            }
-        }
-        useRouteStore.setState({ multiWalletDistribution: _distribution });
-
-        console.log(_distribution, multiWallets);
+  React.useEffect(() => {
+    if (address) {
+      useRouteStore.setState({ multiWallets: [address], gasPayer: address });
     }
+  }, [address, typeof multiWallets == "undefined"]);
 
-    React.useEffect(fixDistribution, [address, multiWallets?.join(","), getRoutes()[0]?.id]);
+  const _distribution = { ...multiWalletDistribution };
 
-    const onClick = async () => {
-        if (!chain || !outputChain || !address || !outputTokens) return;
-        try {
-            const distribution = outputDistribution.map((a, i, arr) => a - (arr[i - 1] ?? 0));
-            distribution.push(100 - distribution.reduce((a, b) => a + b, 0));
+  const fixDistribution = () => {
+    const routes = getRoutes();
+    const inputTokens = routes.map((route) => ({
+      token: route.fromToken,
+      fromAmount: route.fromAmount,
+    }));
+    const total = inputTokens.reduce(
+      (acc, { fromAmount }) => acc + BigInt(fromAmount),
+      0n,
+    );
 
-            await fetchRoutes({
-                inputAmount,
-                fromAddress: address,
-                toAddress: targetAddress,
-                inputChain: chain.id,
-                inputTokens: inputTokens.map(token => ({
-                    address: token.address,
-                    value: token.value,
-                })),
-                outputChain,
-                outputTokens: outputTokens.map(token => ({
-                    address: token.address,
-                    value: token.value,
-                })),
-                distribution,
-                tempAccount,
-                options: {
-                    bridges: allowDenyBridges,
-                    exchanges: allowDenyExchanges,
-                }
-            });
-
-            console.log("Routes fetched");
-
-            setTrials(0);
-        } catch (e) {
-            toast.error("Error fetching routes", {
-                description: <>{e instanceof Error && e.message}<br />Maybe try a different route!</>
-            })
-            if (validUntil && validUntil < new Date()) {
-                setTrials(trials + 1);
+    for (const { token, fromAmount } of inputTokens) {
+      // Distribute the tokens evenly. The bigint represents the token amount, not the percentage
+      const amount = BigInt(fromAmount);
+      const wallets = multiWallets ?? [address!];
+      if (
+        !_distribution[token.address] ||
+        Object.keys(_distribution[token.address]).length !== wallets.length ||
+        !wallets.every((wallet) =>
+          Object.keys(_distribution[token.address]).includes(wallet),
+        ) ||
+        total !==
+          Object.values(_distribution[token.address]).reduce(
+            (acc, val) => acc + val,
+            0n,
+          )
+      ) {
+        _distribution[token.address] = {};
+        if (token.address === zeroAddress) {
+          _distribution[token.address][gasPayer ?? address!] = amount;
+        } else {
+          let leftToSpend = amount;
+          wallets.forEach((wallet, index) => {
+            if (index === wallets.length - 1) {
+              _distribution[token.address][wallet] = leftToSpend;
+            } else {
+              const share = amount / BigInt(wallets.length);
+              _distribution[token.address][wallet] = share;
+              leftToSpend -= share;
             }
+          });
         }
+      }
     }
+    useRouteStore.setState({ multiWalletDistribution: _distribution });
 
-    return <>
-        <Button className="w-full h-full rounded flex justify-center items-center" onClick={onClick} disabled={isFetching}>
-            {isFetching && <Loader2 className="h-6 w-6 animate-spin mx-2" />}
-            Find Routes
-        </Button>
+    console.log(_distribution, multiWallets);
+  };
+
+  React.useEffect(fixDistribution, [
+    address,
+    multiWallets?.join(","),
+    getRoutes()[0]?.id,
+  ]);
+
+  const onClick = async () => {
+    if (!chain || !outputChain || !address || !outputTokens) return;
+    try {
+      const distribution = outputDistribution.map(
+        (a, i, arr) => a - (arr[i - 1] ?? 0),
+      );
+      distribution.push(100 - distribution.reduce((a, b) => a + b, 0));
+
+      await fetchRoutes({
+        inputAmount,
+        fromAddress: address,
+        toAddress: targetAddress,
+        inputChain: chain.id,
+        inputTokens: inputTokens.map((token) => ({
+          address: token.address,
+          value: token.value,
+          image: token.logoURI,
+        })),
+        outputChain,
+        outputTokens: outputTokens.map((token) => ({
+          address: token.address,
+          value: token.value,
+          image: token.logoURI,
+        })),
+        distribution,
+        tempAccount,
+        options: {
+          bridges: allowDenyBridges,
+          exchanges: allowDenyExchanges,
+        },
+      });
+
+      console.log("Routes fetched");
+
+      setTrials(0);
+    } catch (e) {
+      toast.error("Error fetching routes", {
+        description: (
+          <>
+            {e instanceof Error && e.message}
+            <br />
+            Maybe try a different route!
+          </>
+        ),
+      });
+      if (validUntil && validUntil < new Date()) {
+        setTrials(trials + 1);
+      }
+    }
+  };
+
+  return (
+    <>
+      <Button
+        className="w-full h-full rounded flex justify-center items-center"
+        onClick={onClick}
+        disabled={isFetching}
+      >
+        {isFetching && <Loader2 className="h-6 w-6 animate-spin mx-2" />}
+        Find Routes
+      </Button>
     </>
+  );
 }
--- lib/allbridge/stepBuilder.ts
+++ lib/allbridge/stepBuilder.ts
@@ -1,10 +1,10 @@
 // FinalStellarStepBuilder.ts
 
 import {
-	AllbridgeCoreSdk,
-	Messenger,
-	TokenWithChainDetails,
-	ChainDetailsWithTokens,
+  AllbridgeCoreSdk,
+  Messenger,
+  TokenWithChainDetails,
+  ChainDetailsWithTokens,
 } from "@allbridge/bridge-core-sdk";
 import { nanoid } from "nanoid";
 import { type Step, StepTypeEnum } from "@/lib/li.fi-ts";
@@ -12,144 +12,159 @@ import { tokenGet } from "../core/data/tokenLib";
 import { formatUnits, parseUnits, zeroAddress } from "viem";
 import { format } from "path";
 
-export async function FinalAllbridgeStepBuilder({
-	target,
-	fromChainId,
-	toChainId,
-	fromTokenAddress,
-	fromAmount,
-	fromAddress,
-	toAddress,
-}: {
-	target: `0x${string}` | string;
-	fromChainId: number;
-	toChainId: number;
-	fromTokenAddress: `0x${string}` | string;
-	fromAmount: string;
-	fromAddress: `0x${string}` | string;
-	toAddress: `0x${string}` | string;
-}, {
-	sourceToken,
-	destinationToken,
-	destinationChain,
-	sdk,
-}: {
-	sourceToken: TokenWithChainDetails | undefined;
-	destinationToken: TokenWithChainDetails | undefined;
-	sourceChain: ChainDetailsWithTokens;
-	destinationChain: ChainDetailsWithTokens;
-	sdk: AllbridgeCoreSdk;
-}): Promise<Step> {
-	if (!sourceToken || !destinationToken) {
-		throw new Error(`Tokens not supported on the given chains. Source token: ${sourceToken}, destination token: ${destinationToken}`);
-	}
+export async function FinalAllbridgeStepBuilder(
+  {
+    target,
+    fromChainId,
+    toChainId,
+    fromTokenAddress,
+    fromTokenImage,
+    toTokenImage,
+    fromAmount,
+    fromAddress,
+    toAddress,
+  }: {
+    target: `0x${string}` | string;
+    fromChainId: number;
+    toChainId: number;
+    fromTokenAddress: `0x${string}` | string;
+    fromTokenImage?: string;
+    toTokenImage?: string;
+    fromAmount: string;
+    fromAddress: `0x${string}` | string;
+    toAddress: `0x${string}` | string;
+  },
+  {
+    sourceToken,
+    destinationToken,
+    destinationChain,
+    sdk,
+  }: {
+    sourceToken: TokenWithChainDetails | undefined;
+    destinationToken: TokenWithChainDetails | undefined;
+    sourceChain: ChainDetailsWithTokens;
+    destinationChain: ChainDetailsWithTokens;
+    sdk: AllbridgeCoreSdk;
+  },
+): Promise<Step> {
+  if (!sourceToken || !destinationToken) {
+    throw new Error(
+      `Tokens not supported on the given chains. Source token: ${sourceToken}, destination token: ${destinationToken}`,
+    );
+  }
 
-	const steps: Step[] = [];
+  const steps: Step[] = [];
 
-	const fromAmountFloat = formatUnits(BigInt(fromAmount), sourceToken.decimals);
+  const fromAmountFloat = formatUnits(BigInt(fromAmount), sourceToken.decimals);
 
-	const [_amountToBeReceived, gasFeeOptions, nativeToken] = await Promise.all([
-		sdk.getAmountToBeReceived(fromAmountFloat, sourceToken, destinationToken),
-		sdk.getGasFeeOptions(sourceToken, destinationToken, Messenger.ALLBRIDGE),
-		tokenGet(fromChainId, zeroAddress),
-	]);
+  const [_amountToBeReceived, gasFeeOptions, nativeToken] = await Promise.all([
+    sdk.getAmountToBeReceived(fromAmountFloat, sourceToken, destinationToken),
+    sdk.getGasFeeOptions(sourceToken, destinationToken, Messenger.ALLBRIDGE),
+    tokenGet(fromChainId, zeroAddress),
+  ]);
 
-	const amountToBeReceived = parseUnits(_amountToBeReceived, destinationToken.decimals);
+  const amountToBeReceived = parseUnits(
+    _amountToBeReceived,
+    destinationToken.decimals,
+  );
 
-	// Assume we pay gas fee in native currency
-	const gasFee = BigInt(gasFeeOptions.native.int);
+  // Assume we pay gas fee in native currency
+  const gasFee = BigInt(gasFeeOptions.native.int);
 
-	// Compute gas fee amount in USD
-	const gasFeeAmountUSD = Number(gasFeeOptions.stablecoin?.float);
+  // Compute gas fee amount in USD
+  const gasFeeAmountUSD = Number(gasFeeOptions.stablecoin?.float);
 
-	// Get average transfer time
-	const transferTimeMs = sdk.getAverageTransferTime(
-		sourceToken,
-		destinationToken,
-		Messenger.ALLBRIDGE,
-	);
+  // Get average transfer time
+  const transferTimeMs = sdk.getAverageTransferTime(
+    sourceToken,
+    destinationToken,
+    Messenger.ALLBRIDGE,
+  );
 
-	const executionDuration = (transferTimeMs ?? 1000000) / 1000; // Convert ms to seconds
+  const executionDuration = (transferTimeMs ?? 1000000) / 1000; // Convert ms to seconds
 
-	// // Build send transaction
-	// const rawTransactionSend = await sdk.bridge.rawTxBuilder.send({
-	// 	amount: fromAmount,
-	// 	fromAccountAddress: fromAddress,
-	// 	toAccountAddress: toAddress,
-	// 	sourceToken: sourceToken,
-	// 	destinationToken: destinationToken,
-	// 	messenger: Messenger.ALLBRIDGE,
-	// });
+  // // Build send transaction
+  // const rawTransactionSend = await sdk.bridge.rawTxBuilder.send({
+  // 	amount: fromAmount,
+  // 	fromAccountAddress: fromAddress,
+  // 	toAccountAddress: toAddress,
+  // 	sourceToken: sourceToken,
+  // 	destinationToken: destinationToken,
+  // 	messenger: Messenger.ALLBRIDGE,
+  // });
 
+  const approvalAddress = sourceToken.bridgeAddress;
 
-	const approvalAddress = sourceToken.bridgeAddress;
+  // Add send step
+  steps.push({
+    id: nanoid(),
+    type: StepTypeEnum.Cross,
+    action: {
+      fromAmount: fromAmount,
+      fromChainId: fromChainId,
+      fromToken: {
+        address: sourceToken.tokenAddress,
+        symbol: sourceToken.symbol,
+        decimals: sourceToken.decimals,
+        chainId: fromChainId,
+        name: sourceToken.name,
+        logoURI: fromTokenImage,
+      },
+      toChainId: destinationChain.chainId
+        ? Number.parseInt(destinationChain.chainId.split("0x")[1], 16)
+        : toChainId,
+      toToken: {
+        address: destinationToken.tokenAddress,
+        symbol: destinationToken.symbol,
+        decimals: destinationToken.decimals,
+        chainId: destinationChain.chainId
+          ? Number.parseInt(destinationChain.chainId.split("0x")[1], 16)
+          : toChainId,
+        name: destinationToken.name,
+        logoURI: toTokenImage,
+      },
+      fromAddress: fromAddress,
+      toAddress: toAddress,
+      slippage: 0,
+    },
+    estimate: {
+      approvalAddress,
+      fromAmount: fromAmount,
+      toAmount: amountToBeReceived.toString(),
+      toAmountMin: amountToBeReceived.toString(), // Adjust for slippage if needed
+      tool: "Allbridge",
+      feeCosts: [
+        {
+          amount: gasFee.toString(),
+          amountUSD: gasFeeAmountUSD.toString(),
+          included: true,
+          name: "Gas Fee",
+          percentage: ((Number(gasFee) / Number(fromAmount)) * 100).toString(),
+          token: nativeToken,
+          description: "Gas fee for transaction",
+        },
+      ],
+      executionDuration: executionDuration,
+      gasCosts: [
+        {
+          amount: gasFee.toString(),
+          type: "SEND",
+          token: nativeToken,
+          amountUSD: gasFeeAmountUSD.toString(),
+          estimate: gasFee.toString(),
+          price: "0", // Need to get gas price if available
+        },
+      ],
+    },
+    tool: "Allbridge",
+    toolDetails: {
+      key: "allbridge",
+      name: "Allbridge",
+      logoURI:
+        "https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/bridges/allbridge.png",
+    },
+  });
 
-	// Add send step
-	steps.push({
-		id: nanoid(),
-		type: StepTypeEnum.Cross,
-		action: {
-			fromAmount: fromAmount,
-			fromChainId: fromChainId,
-			fromToken: {
-				address: sourceToken.tokenAddress,
-				symbol: sourceToken.symbol,
-				decimals: sourceToken.decimals,
-				chainId: fromChainId,
-				name: sourceToken.name,
-			},
-			toChainId: destinationChain.chainId ? Number.parseInt(destinationChain.chainId.split("0x")[1], 16) : toChainId,
-			toToken: {
-				address: destinationToken.tokenAddress,
-				symbol: destinationToken.symbol,
-				decimals: destinationToken.decimals,
-				chainId: destinationChain.chainId ? Number.parseInt(destinationChain.chainId.split("0x")[1], 16) : toChainId,
-				name: destinationToken.name,
-			},
-			fromAddress: fromAddress,
-			toAddress: toAddress,
-			slippage: 0,
-		},
-		estimate: {
-			approvalAddress,
-			fromAmount: fromAmount,
-			toAmount: amountToBeReceived.toString(),
-			toAmountMin: amountToBeReceived.toString(), // Adjust for slippage if needed
-			tool: "Allbridge",
-			feeCosts: [
-				{
-					amount: gasFee.toString(),
-					amountUSD: gasFeeAmountUSD.toString(),
-					included: true,
-					name: "Gas Fee",
-					percentage: (
-						(Number(gasFee) / Number(fromAmount)) *
-						100
-					).toString(),
-					token: nativeToken,
-					description: "Gas fee for transaction",
-				},
-			],
-			executionDuration: executionDuration,
-			gasCosts: [
-				{
-					amount: gasFee.toString(),
-					type: "SEND",
-					token: nativeToken,
-					amountUSD: gasFeeAmountUSD.toString(),
-					estimate: gasFee.toString(),
-					price: "0", // Need to get gas price if available
-				},
-			],
-		},
-		tool: "Allbridge",
-		toolDetails: {
-			key: "allbridge",
-			name: "Allbridge",
-			logoURI: "/icons/allbridge.svg",
-		},
-	});
-
-	// Return the final step
-	return steps[steps.length - 1];
+  // Return the final step
+  return steps[steps.length - 1];
 }
