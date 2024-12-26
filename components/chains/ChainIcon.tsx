50487754c490578a3e3e1aab83f513f955b3404e
Arthur Guiot <arguiot@gmail.com>
2024-12-26 14:13:53 +0100

    improved hedera integration

--- components/chains/ChainIcon.tsx
+++ components/chains/ChainIcon.tsx
@@ -1,4 +1,4 @@
-import { StellarChainId } from "@/lib/layerzero/aptos/omnichains";
+import { HashportChainId, StellarChainId } from "@/lib/layerzero/aptos/omnichains";
 import { ChainIcon } from "connectkit";
 import Image from "next/image";
 
@@ -6,6 +6,16 @@ export default function ChainIcons({ id }: { id: number }) {
   if (id === StellarChainId) {
     return (
       <Image src="/icons/stellar.svg" width={20} height={20} alt="Stellar" />
+    );
+  }
+  if (id === HashportChainId) {
+    return (
+      <img
+        src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/hedera/info/logo.png"
+        width={20}
+        height={20}
+        alt="Hedera"
+      />
     );
   }
   return <ChainIcon id={id} />;
