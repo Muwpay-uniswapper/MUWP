85a119544cfcea56f2db76f2fea5117ff1a4ab1c
Arthur Guiot <arguiot@gmail.com>
2025-05-29 10:25:23 -0700

    fix: network tabs

--- components/networks/networks.tsx
+++ components/networks/networks.tsx
@@ -1,11 +1,13 @@
-import api from '@/lib/core/data/api';
-import { BridgesList } from './bridges';
-import { ExchangesList } from './exchanges';
+import api from "@/lib/core/data/api";
+import { BridgesList } from "./bridges";
+import { ExchangesList } from "./exchanges";
 
 export async function Networks() {
-    const data = await api.toolsGet()
-    return <div className="flex flex-row gap-4">
-        <BridgesList bridges={data.bridges ?? []} />
-        <ExchangesList data={data.exchanges ?? []} />
+  const data = await api.toolsGet();
+  return (
+    <div className="flex flex-col lg:flex-row gap-4 max-w-[100vw]">
+      <BridgesList bridges={data.bridges ?? []} />
+      <ExchangesList data={data.exchanges ?? []} />
     </div>
+  );
 }
