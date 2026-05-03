c2f3cdf971521a672b0e38feb2c8a0b0f5c92ce4
Cedric <cedric@smartt.dev>
2026-05-03 15:36:05 +0200

    fix: tests run in ci

--- apps/web/test-back/account.test.ts
+++ apps/web/test-back/account.test.ts
@@ -1,27 +1,36 @@
-import { HDKey, hdKeyToAccount } from 'viem/accounts'
+import { describe, expect, it } from "bun:test";
 import { fromHex } from "viem";
-import { describe, it, expect } from "bun:test";
+import { HDKey, hdKeyToAccount } from "viem/accounts";
 
-describe("HD Wallet", function () {
-    it("Should generate a consistent wallet", async function () {
-        // const privateKey = secp256k1.utils.randomPrivateKey()
-        const privateKey = new Uint8Array([166, 61, 111, 154, 173, 169, 54, 1, 200, 83, 16, 218, 182, 75, 241, 138, 223, 240, 253, 49, 165, 125, 223, 26, 46, 207, 124, 64, 212, 206, 187, 139])
-        const hdKey = HDKey.fromMasterSeed(privateKey)
-        const account = hdKeyToAccount(hdKey, {
-            accountIndex: 0,
-        })
+describe("HD Wallet", () => {
+	it("Should generate a consistent wallet", async () => {
+		// const privateKey = secp256k1.utils.randomPrivateKey()
+		const privateKey = new Uint8Array([
+			166, 61, 111, 154, 173, 169, 54, 1, 200, 83, 16, 218, 182, 75, 241, 138,
+			223, 240, 253, 49, 165, 125, 223, 26, 46, 207, 124, 64, 212, 206, 187,
+			139,
+		]);
+		const hdKey = HDKey.fromMasterSeed(privateKey);
+		const account = hdKeyToAccount(hdKey, {
+			accountIndex: 0,
+		});
 
-        expect(account.address).toEqual("0xE05f5dE81F34C461B3fe7e135B8314eACfCA3F3a")
-    });
+		expect(account.address).toEqual(
+			"0xE05f5dE81F34C461B3fe7e135B8314eACfCA3F3a",
+		);
+	});
 
-    it("Should generate a consistent wallet from env", async function () {
-        const master_hd = process.env.MASTER_HD?.trim() as `0x${string}`
-        const privateKey = fromHex(master_hd, "bytes")
-        const hdKey = HDKey.fromMasterSeed(privateKey)
-        const account = hdKeyToAccount(hdKey, { accountIndex: 0 })
-        const account2 = hdKeyToAccount(HDKey.fromMasterSeed(privateKey), { accountIndex: 0 })
+	it("Should generate a consistent wallet from env", async () => {
+		const master_hd = process.env.MASTER_HD?.trim();
+		if (!master_hd) return;
+		const privateKey = fromHex(master_hd as `0x${string}`, "bytes");
+		const hdKey = HDKey.fromMasterSeed(privateKey);
+		const account = hdKeyToAccount(hdKey, { accountIndex: 0 });
+		const account2 = hdKeyToAccount(HDKey.fromMasterSeed(privateKey), {
+			accountIndex: 0,
+		});
 
-        expect(account.address).toMatch(/^0x[0-9a-fA-F]{40}$/)
-        expect(account.address).toEqual(account2.address)
-    })
+		expect(account.address).toMatch(/^0x[0-9a-fA-F]{40}$/);
+		expect(account.address).toEqual(account2.address);
+	});
 });
